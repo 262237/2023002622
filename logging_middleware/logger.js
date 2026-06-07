206f3eb91fbc2860afcea0f7323b2978fb480dc6
J Vandana <155727321+262237@users.noreply.github.com>
2026-06-07 11:28:23 +0530

    Create logger.js

---
+++ logging_middleware/logger.js
@@ -0,0 +1,26 @@
+import { TOKEN } from "./config.js";
+
+export async function Log(stack, level, packageName, message) {
+  try {
+    const response = await fetch(
+      "http://4.224.186.213/evaluation-service/logs",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          stack,
+          level,
+          package: packageName,
+          message
+        })
+      }
+    );
+
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
